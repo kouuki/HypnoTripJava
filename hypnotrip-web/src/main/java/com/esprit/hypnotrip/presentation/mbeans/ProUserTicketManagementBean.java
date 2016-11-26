4ae17b8ba3768d01852ca3e5b794587093a047f1
Amin <amin.ghorbel@esprit.tn>
2016-11-26 21:42:18 +0100

    Add Ticket management to pro the pro user

--- hypnotrip-web/src/main/java/com/esprit/hypnotrip/presentation/mbeans/ProUserTicketManagementBean.java
+++ hypnotrip-web/src/main/java/com/esprit/hypnotrip/presentation/mbeans/ProUserTicketManagementBean.java
@@ -1,18 +1,21 @@
 package com.esprit.hypnotrip.presentation.mbeans;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import com.esprit.hypnotrip.persistence.Pages;
 import com.esprit.hypnotrip.persistence.Tickets;
 import com.esprit.hypnotrip.persistence.User;
 import com.esprit.hypnotrip.services.exceptions.EventOverException;
 import com.esprit.hypnotrip.services.exceptions.TicketAlreadyBookedException;
+import com.esprit.hypnotrip.services.interfaces.PageServiceLocal;
 import com.esprit.hypnotrip.services.interfaces.TicketServicesLocal;
 
 @ViewScoped
@@ -22,27 +25,44 @@ public class ProUserTicketManagementBean {
 	private List<Tickets> tickets;
 	private Tickets selectedTicket = new Tickets();
 	private Tickets bestBookedTicket = new Tickets();
-	private Pages event = new Pages() ; 
-	@ManagedProperty(value="#{loginBean}")
-	private LoginBean loginBean ; 
-	private User user; 
-	
+	private Integer idEvent;
+	private Pages event = new Pages();
+	@ManagedProperty(value = "#{loginBean}")
+	private LoginBean loginBean;
+	private User user;
+
 	private boolean showCreateUpdateForm = false;
 	private boolean listAllTickets = true;
-	private boolean displayMostBookedTicket = false ; 
+	private boolean displayMostBookedTicket = false;
 
 	@EJB
 	private TicketServicesLocal ticketServicesLocal;
+	@EJB
+	private PageServiceLocal pageServiceLocal  ;
 
 	@PostConstruct
 	public void init() {
-		this.user = loginBean.getUser();
-		event.setPageId(11);
-		tickets = ticketServicesLocal.selectAllTicketsByIdEvent(event.getPageId());
-		bestBookedTicket = ticketServicesLocal.mostBookedTicketByEvent(event);
-		if (bestBookedTicket!=null) {
-			displayMostBookedTicket = true; 
+
+		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		try{
+			
+		
+			idEvent = Integer.parseInt(params.get("idEvent"));
+			this.user = loginBean.getUser();
+	
+			event = pageServiceLocal.findPageById(idEvent);
+			tickets = ticketServicesLocal.selectAllTicketsByIdEvent(event.getPageId());
+			bestBookedTicket = ticketServicesLocal.mostBookedTicketByEvent(event);
+			if (bestBookedTicket != null) {
+				displayMostBookedTicket = true;
+			}
+		
+		
+		
+		}catch(Exception e) {
+			
 		}
+
 	}
 
 	public String doAddOrUpdateTicket() {
@@ -52,7 +72,7 @@ public class ProUserTicketManagementBean {
 		} catch (EventOverException | TicketAlreadyBookedException e) {
 			e.printStackTrace();
 		}
-		return "manageTickets?faces-redirect=true";
+		return "manageTickets?faces-redirect=false&includeViewParams=true";
 	}
 
 	public String doDeleteTicket() {
@@ -63,19 +83,18 @@ public class ProUserTicketManagementBean {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return "manageTickets?faces-redirect=true";
+		return "manageTickets?faces-redirect=true&includeViewParams=true";
 
 	}
 
 	public Long numberOfTicketsBooked(Tickets ticket) {
 
-		if (ticket!=null) {
-			 return ticketServicesLocal.numberOfTicketsBookedByIdTicket(ticket.getTicketId());
-		} else  {
-			return 0L ;
+		if (ticket != null) {
+			return ticketServicesLocal.numberOfTicketsBookedByIdTicket(ticket.getTicketId());
+		} else {
+			return 0L;
 		}
-			
-	
+
 	}
 
 	public String selectTicket() {
@@ -172,5 +191,25 @@ public class ProUserTicketManagementBean {
 	public void setUser(User user) {
 		this.user = user;
 	}
+
+	public PageServiceLocal getPageServiceLocal() {
+		return pageServiceLocal;
+	}
+
+	public void setPageServiceLocal(PageServiceLocal pageServiceLocal) {
+		this.pageServiceLocal = pageServiceLocal;
+	}
+
+	public Integer getIdEvent() {
+		return idEvent;
+	}
+
+	public void setIdEvent(Integer idEvent) {
+		this.idEvent = idEvent;
+	}
+
+	
+	
+
 
 }
