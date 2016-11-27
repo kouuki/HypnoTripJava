111090e5d5a126a28d204b3264b14226cf357bc8
Karim ben romdhane <karim.b.romdhane@gmail.com>
2016-11-27 18:29:13 +0100

    Adding some values for tommorow's demo

--- hypnotrip-ejb/src/main/java/com/esprit/hypnotrip/services/util/DefaultDB.java
+++ hypnotrip-ejb/src/main/java/com/esprit/hypnotrip/services/util/DefaultDB.java
@@ -13,12 +13,15 @@ import javax.ejb.Startup;
 import com.esprit.hypnotrip.persistence.Event;
 import com.esprit.hypnotrip.persistence.Offer;
 import com.esprit.hypnotrip.persistence.Pages;
+import com.esprit.hypnotrip.persistence.Rates;
+import com.esprit.hypnotrip.persistence.RatesId;
 import com.esprit.hypnotrip.persistence.Touristicplace;
 import com.esprit.hypnotrip.persistence.User;
 import com.esprit.hypnotrip.services.exceptions.EventOverException;
 import com.esprit.hypnotrip.services.exceptions.TicketAlreadyBookedException;
 import com.esprit.hypnotrip.services.interfaces.OfferServiceLocal;
 import com.esprit.hypnotrip.services.interfaces.PageServiceLocal;
+import com.esprit.hypnotrip.services.interfaces.RateServiceLocal;
 import com.esprit.hypnotrip.services.interfaces.TicketServicesLocal;
 import com.esprit.hypnotrip.services.interfaces.ToursiticPlaceServiceLocal;
 import com.esprit.hypnotrip.services.interfaces.UserServicesLocal;
@@ -38,6 +41,8 @@ public class DefaultDB {
 	ToursiticPlaceServiceLocal toursiticPlaceServiceLocal;
 	@EJB
 	TicketServicesLocal ticketServicesLocal;
+	@EJB
+	RateServiceLocal rateServiceLocal;
 
 	public DefaultDB() {
 		// TODO Auto-generated constructor stub
@@ -179,9 +184,91 @@ public class DefaultDB {
 		pageServiceLocal.saveOrUpdatePage(event3, "b38f3299-6949-42c7-9a6c-f998c66f485d");
 
 		// ********************************************************************************************************************
+		// ***********************************RATING AREA FOR DATABASE****************************************************
 		// ********************************************************************************************************************
-		// ********************************************************************************************************************
-
+		RatesId id = new RatesId();
+		id.setId("b38f3299-6949-42c7-9a6c-f998c66f4852");
+		id.setpageId(4);
+		Rates rates = new Rates();
+		rates.setdateRating(new Date());
+		rates.setNiveau(3);
+		rates.setId(id);
+		
+		RatesId id2 = new RatesId();
+		id2.setId("b38f3299-6949-42c7-9a6c-f998c66a258f");
+		id2.setpageId(4);
+		Rates rates2 = new Rates();
+		rates2.setdateRating(new Date());
+		rates2.setNiveau(5);
+		rates2.setId(id2);
+		
+		RatesId id3 = new RatesId();
+		id3.setId("b38f3299-6949-42c7-9a6c-f998c666658f");
+		id3.setpageId(4);
+		Rates rates3 = new Rates();
+		rates3.setdateRating(new Date());
+		rates3.setNiveau(2);
+		rates3.setId(id3);
+		
+		
+		RatesId id6 = new RatesId();
+		id6.setId("b38f3299-6949-42c7-9a6c-f998c66f4852");
+		id6.setpageId(5);
+		Rates rates6 = new Rates();
+		rates6.setdateRating(new Date());
+		rates6.setNiveau(3);
+		rates6.setId(id6);
+		
+		RatesId id4 = new RatesId();
+		id4.setId("b38f3299-6949-42c7-9a6c-f998c66a258f");
+		id4.setpageId(11);
+		Rates rates4 = new Rates();
+		rates4.setdateRating(new Date());
+		rates4.setNiveau(2);
+		rates4.setId(id4);
+		
+		RatesId id5 = new RatesId();
+		id5.setId("b38f3299-6949-42c7-9a6c-f998c666658f");
+		id5.setpageId(7);
+		Rates rates5 = new Rates();
+		rates5.setdateRating(new Date());
+		rates5.setNiveau(1);
+		rates5.setId(id5);
+		
+		RatesId id9 = new RatesId();
+		id9.setId("b38f3299-6949-42c7-9a6c-f998c66f4852");
+		id9.setpageId(5);
+		Rates rates9 = new Rates();
+		rates9.setdateRating(new Date());
+		rates9.setNiveau(3);
+		rates9.setId(id9);
+		
+		RatesId id7 = new RatesId();
+		id7.setId("b38f3299-6949-42c7-9a6c-f998c66a258f");
+		id7.setpageId(11);
+		Rates rates7 = new Rates();
+		rates7.setdateRating(new Date());
+		rates7.setNiveau(5);
+		rates7.setId(id7);
+		
+		RatesId id8 = new RatesId();
+		id8.setId("b38f3299-6949-42c7-9a6c-f998c666658f");
+		id8.setpageId(7);
+		Rates rates8 = new Rates();
+		rates8.setdateRating(new Date());
+		rates8.setNiveau(2);
+		rates8.setId(id8);
+		
+		rateServiceLocal.addRate(rates);
+		rateServiceLocal.addRate(rates2);
+		rateServiceLocal.addRate(rates3);
+		rateServiceLocal.addRate(rates4);
+		rateServiceLocal.addRate(rates5);
+		rateServiceLocal.addRate(rates6);
+		rateServiceLocal.addRate(rates7);
+		rateServiceLocal.addRate(rates8);
+		rateServiceLocal.addRate(rates9);
+	
 	}
 
 }
